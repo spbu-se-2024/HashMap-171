bfbb97cd1c47daa85ff62891885c4fa42e5291bf
Stepan <st085873@gmail.com>
2020-11-22 17:54:47 +0300

    Первая рабочая версия SHA-1

--- SHA algorithm/main.c
+++ SHA algorithm/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "SHA.h"
 
 int main() {
-    printf("Hello, World!\n");
+
+    char string[128] = "hello worldhello worldhello worldhello worldhello worlddddddddddhello worldhello worldhello worldhello worldhello worldddddddddd";
+    unsigned int hash[5];
+
+    for(unsigned i = 0; i < 128; i++) {
+        printf("%2u : %2.2x\n", i, string[i]);
+    }
+    printf("\n");
+
+   SHA_1(string, 128, hash);
+    for(unsigned i = 0; i < 5; i++) {
+        printf("%x ", hash[i]);
+    }
+
     return 0;
 }
