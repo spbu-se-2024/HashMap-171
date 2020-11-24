f7fff00218bfcef892b1e3c2e6e370180a9a7409
Azim Muradov <azim.muradov.dev@gmail.com>
2020-11-25 00:26:08 +0300

    Add Bst_printErr(...) function

--- src/main/libs/bst/bst_err.h
+++ src/main/libs/bst/bst_err.h
@@ -1,6 +1,8 @@
 #ifndef HASHMAP_171_BST_ERR_H
 #define HASHMAP_171_BST_ERR_H
 
+#include <stdio.h>
+
 typedef enum {
     BST_E_OK,
     BST_E_MEM_ALLOC,
@@ -8,5 +10,11 @@ typedef enum {
     BST_E_NULL_ARG,
     BST_E_OTHER,
 } BstErrCode;
+
+// TODO : Add appropriate messages for error codes
+static char *BST_ERR_MESSAGES[] = {};
+
+
+void Bst_printErr(FILE *errStream, BstErrCode errCode);
 
 #endif // HASHMAP_171_BST_ERR_H
