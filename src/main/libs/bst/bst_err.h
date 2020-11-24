3235aa143229ef5a1f84fc80714b9586f3f3a390
Azim Muradov <azim.muradov.dev@gmail.com>
2020-11-24 21:34:38 +0300

    Add BST draft

---
+++ src/main/libs/bst/bst_err.h
@@ -0,0 +1,12 @@
+#ifndef HASHMAP_171_BST_ERR_H
+#define HASHMAP_171_BST_ERR_H
+
+typedef enum {
+    BST_E_OK,
+    BST_E_MEM_ALLOC,
+    BST_E_NULL_THIS,
+    BST_E_NULL_ARG,
+    BST_E_OTHER,
+} BstErrCode;
+
+#endif // HASHMAP_171_BST_ERR_H
