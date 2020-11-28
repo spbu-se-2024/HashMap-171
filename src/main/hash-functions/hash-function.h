2ecca563704ef08a18fedf5ee6a501e4ec647063
Stepan <st085873@gmail.com>
2020-11-28 19:38:04 +0300

    Led sha-1 to a single interface

--- src/main/hash-functions/hash-function.h
+++ src/main/hash-functions/hash-function.h
@@ -3,6 +3,13 @@
 
 #include <stddef.h>
 
-typedef void (*HashFunction)(const char *message, size_t size, unsigned long long *hash);
+enum HashError {
+    OK,
+    ALLOCATION_ERROR
+};
+
+typedef enum HashError (*HashFunction)(const char *message, size_t size, unsigned int *hash);
+
+enum HashError SHA_1(const char *message, size_t size, unsigned int *hash);
 
 #endif // HASHMAP_171_HASH_FUNCTION_H
