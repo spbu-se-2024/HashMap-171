4c87431a9cc28a1c59a4312636fe0c8464accc0c
TimPushkin <pushkin.td@gmail.com>
2020-11-29 13:57:46 +0300

    Added polynomial hashing

--- src/main/hash-functions/hash-function.h
+++ src/main/hash-functions/hash-function.h
@@ -12,4 +12,6 @@ typedef enum HashError (*HashFunction)(const char *message, size_t size, unsigne
 
 enum HashError SHA_1(const char *message, size_t size, unsigned int *hash);
 
+enum HashError polynomial(const char *message, size_t size, unsigned int *hash);
+
 #endif // HASHMAP_171_HASH_FUNCTION_H
