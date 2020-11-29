3e4775ee4294705c2292a71655592b3420fb9727
Azim Muradov <azim.muradov.dev@gmail.com>
2020-11-30 00:29:52 +0300

    Clean up hash-function code

--- src/main/hash-functions/hash-function.h
+++ src/main/hash-functions/hash-function.h
@@ -3,15 +3,17 @@
 
 #include <stddef.h>
 
-enum HashError {
-    OK,
-    ALLOCATION_ERROR
-};
+typedef enum {
+    HASH_FUNC_E_OK,
+    HASH_FUNC_E_MEM_ALLOC,
+} HashFuncErrCode;
 
-typedef enum HashError (*HashFunction)(const char *message, size_t size, unsigned int *hash);
+typedef HashFuncErrCode (*HashFunction)(const char *message, size_t size, unsigned int *hash);
 
-enum HashError SHA_1(const char *message, size_t size, unsigned int *hash);
+HashFuncErrCode SHA_1(const char *message, size_t size, unsigned int *hash);
 
-enum HashError polynomial(const char *message, size_t size, unsigned int *hash);
+HashFuncErrCode MD5(const char *message, size_t size, unsigned int *hash);
+
+HashFuncErrCode polynomial(const char *message, size_t size, unsigned int *hash);
 
 #endif // HASHMAP_171_HASH_FUNCTION_H
--- src/main/hash-functions/polynomial.c
+++ src/main/hash-functions/polynomial.c
@@ -1,13 +1,13 @@
-#include <stdint.h>
-
 #include "hash-function.h"
+
+#include <stdint.h>
 
 #define CEIL_DIV(x, y) (1 + (x - 1) / y)
 
 #define MULT_PRIME 257 // A const prime by which chars are multiplied
 #define UINT_BIT_MASK ((uint64_t) ((2ul << (sizeof(unsigned int) * 8)) - 1ul)) // Masks off everything but num of bits needed to store an unsigned int
 
-enum HashError polynomial(const char *message, size_t size, unsigned int *hash) {
+HashFuncErrCode polynomial(const char *message, size_t size, unsigned int *hash) {
     uint64_t hashBuf = 0;
     for (unsigned int i = 0; i < size; i++) {
         hashBuf = hashBuf * MULT_PRIME + message[i];
@@ -16,5 +16,5 @@ enum HashError polynomial(const char *message, size_t size, unsigned int *hash) 
     for (unsigned int i = 0; i < blocksNum; i++) {
         hash[blocksNum - 1 - i] = hashBuf & (UINT_BIT_MASK << sizeof(unsigned int) * 8 * i);
     }
-    return OK;
+    return HASH_FUNC_E_OK;
 }
