89abf40a42a1c57a48498bdda0f1cc8a4b35ecfe
TimPushkin <pushkin.td@gmail.com>
2020-11-30 20:16:02 +0300

    Replaced unsigned int in hash array with uint32_t

--- src/main/hash-functions/polynomial.c
+++ src/main/hash-functions/polynomial.c
@@ -8,7 +8,7 @@
 #define UINT32_BIT_SIZE (sizeof(uint32_t) * CHAR_BIT)               // Size of uint32_t in bits (equals 32)
 #define UINT32_BIT_MASK ((uint64_t) ((1ul << UINT32_BIT_SIZE) - 1)) // Masks off everything but right-most 32 bits
 
-HashFuncErrCode polynomial(const char *message, size_t size, unsigned int *hash) {
+HashFuncErrCode polynomial(const char *message, size_t size, uint32_t *hash) {
     uint64_t hashBuf = 0;
     for (unsigned int i = 0; i < size; i++) {
         hashBuf = hashBuf * MULT_PRIME + message[i];
