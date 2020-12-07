fa13f0f43e3edbcf07061c9eb0addcc03880767b
Azim Muradov <azim.muradov.dev@gmail.com>
2020-12-07 23:52:17 +0300

    Apply error-handling.h to AVL Tree

--- src/main/avl-tree/avl-tree-err.c
+++ src/main/avl-tree/avl-tree-err.c
@@ -1,12 +1,10 @@
 #include "avl-tree-err.h"
 
 
-// TODO : Add appropriate messages for error codes
 const char *const AVL_TREE_ERR_MESSAGES[] = {
-    "TODO",
+    "everything is ok",
+    "memory allocation error",
+    "pointer to `this` must not be NULL",
+    "the function argument must not be NULL",
+    "something went wrong",
 };
-
-
-void AvlTree_printErr(FILE *errStream, AvlTreeErrCode errCode) {
-    fprintf(errStream, "AVL Tree - Error: %s\n", AVL_TREE_ERR_MESSAGES[errCode]);
-}
--- src/main/avl-tree/avl-tree-err.h
+++ src/main/avl-tree/avl-tree-err.h
@@ -1,8 +1,11 @@
 #ifndef HASHMAP_171_AVL_TREE_ERR_H
 #define HASHMAP_171_AVL_TREE_ERR_H
 
-#include <stdio.h>
+#include "avl-tree-config.h"
+#include "../error-handling/error-handling.h"
 
+
+/*----------------------------------------------- AVL Tree Error Codes -----------------------------------------------*/
 
 typedef enum {
     AVL_TREE_E_OK,
@@ -15,7 +18,47 @@ typedef enum {
 extern const char *const AVL_TREE_ERR_MESSAGES[];
 
 
-void AvlTree_printErr(FILE *errStream, AvlTreeErrCode errCode);
+/*----------------------------------------- AVL Tree Error Handling Interface ----------------------------------------*/
+
+#define AvlTree_printErr(errStream, errCode) \
+    ErrorHandling_printErr(                  \
+        AvlTree, AVL_TREE, "AVL Tree"        \
+        errStream, errCode                   \
+    )
+
+#define AvlTree_autoprintErr(errCode)                  \
+    do {                                               \
+        if (AVL_TREE_OPT_1_AUTOPRINT_ERR) {            \
+            ErrorHandling_printErr(                    \
+                AvlTree, AVL_TREE, "AVL Tree",         \
+                AVL_TREE_OPT_1_ARG_ERR_STREAM, errCode \
+            );                                         \
+        }                                              \
+    } while(0)
+
+
+#define AvlTree_stopRunOnBadErrCode(errCode, codeToRunOnBadErrCode) \
+    ErrorHandling_stopRunOnBadErrCode(                              \
+        AvlTree, AVL_TREE, "AVL Tree",                              \
+        errCode, codeToRunOnBadErrCode                              \
+    )
+
+
+#define AvlTree_printErrAndStopRunIf(isErr, errStream, errCode, codeToRunIfIsErr) \
+    ErrorHandling_printErrAndStopRunIf(                                           \
+        AvlTree, AVL_TREE, "AVL Tree"                                             \
+        isErr, errStream, errCode, codeToRunIfIsErr                               \
+    )
+
+#define AvlTree_autoprintErrAndStopRunIf(isErr, errCode, codeToRunIfIsErr)      \
+    do {                                                                        \
+        if (AVL_TREE_OPT_1_AUTOPRINT_ERR) {                                     \
+            ErrorHandling_printErrAndStopRunIf(                                 \
+                AvlTree, AVL_TREE, "AVL Tree",                                  \
+                isErr, AVL_TREE_OPT_1_ARG_ERR_STREAM, errCode, codeToRunIfIsErr \
+            );                                                                  \
+        }                                                                       \
+    } while(0)
 
 
 #endif // HASHMAP_171_AVL_TREE_ERR_H
