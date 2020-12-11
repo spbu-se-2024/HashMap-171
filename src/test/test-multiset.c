3ae58253cfee1e929cc5f8970597bffecfb95a7b
TimPushkin <pushkin.td@gmail.com>
2020-12-11 13:31:14 +0300

    Rewrote tests for multiset to use heap

--- src/test/test-multiset.c
+++ src/test/test-multiset.c
@@ -2,6 +2,7 @@
 
 #include "../main/multiset/multiset.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 // Config for multiset, which is applied in the tests
@@ -32,8 +33,15 @@ static void TestMultiset_AddCount_EqualsAvlTreeCount(CuTest *tc) {
     MultisetConfig config = MULTISET_CONFIG;
     Multiset_initMultiset(&multiset, config);
 
-    MultisetItem strs[4] = {"abc", "a", " ", "0"};
-    const size_t strsNum = sizeof(strs) / sizeof(strs[0]);
+    const size_t strsNum = 4, maxCStrLen = 4;
+    char **strs = malloc(strsNum * sizeof(char*));
+    for (size_t i = 0; i < strsNum; i++) {
+        strs[i] = malloc(maxCStrLen * sizeof(char));
+    }
+    strcpy(strs[0], "abc");
+    strcpy(strs[1], "a");
+    strcpy(strs[2], " ");
+    strcpy(strs[3], "0");
 
     CuAssertIntEquals(tc, 0, multiset.itemsCount);
     CuAssertIntEquals(tc, 0, countTreesItems(&multiset));
@@ -42,7 +50,7 @@ static void TestMultiset_AddCount_EqualsAvlTreeCount(CuTest *tc) {
         CuAssertTrue(tc, multiset.itemsCount == i + 1 && multiset.itemsCount == countTreesItems(&multiset));
     }
 
-    Multiset_eraseMultiset(&multiset);
+    CuAssertIntEquals(tc, MULTISET_E_OK, Multiset_eraseMultiset(&multiset));
 }
 
 static void TestMultiset_AddCountUnique_EqualsAvlTreeCountUnique(CuTest *tc) {
@@ -50,8 +58,16 @@ static void TestMultiset_AddCountUnique_EqualsAvlTreeCountUnique(CuTest *tc) {
     MultisetConfig config = MULTISET_CONFIG;
     Multiset_initMultiset(&multiset, config);
 
-    MultisetItem strs[5] = {"-", "cdeeeG", " ", "123", "cdeeeG"};
-    const size_t strsNum = sizeof(strs) / sizeof(strs[0]);
+    const size_t strsNum = 5, maxCStrLen = 7;
+    char **strs = malloc(strsNum * sizeof(char*));
+    for (size_t i = 0; i < strsNum; i++) {
+        strs[i] = malloc(maxCStrLen * sizeof(char));
+    }
+    strcpy(strs[0], "-");
+    strcpy(strs[1], "cdeeeG");
+    strcpy(strs[2], " ");
+    strcpy(strs[3], "123");
+    strcpy(strs[4], "cdeeeG");
 
     CuAssertIntEquals(tc, 0, multiset.uniqueItemsCount);
     CuAssertIntEquals(tc, 0, countTreesUniqueItems(&multiset));
@@ -64,7 +80,7 @@ static void TestMultiset_AddCountUnique_EqualsAvlTreeCountUnique(CuTest *tc) {
     CuAssertTrue(tc, multiset.uniqueItemsCount == strsNum - 1 &&
                      multiset.uniqueItemsCount == countTreesUniqueItems(&multiset));
 
-    Multiset_eraseMultiset(&multiset);
+    CuAssertIntEquals(tc, MULTISET_E_OK, Multiset_eraseMultiset(&multiset));
 }
 
 static void TestMultiset_AddAllCharsCountMixed_EqualsAvlTreeCountMixed(CuTest *tc) {
@@ -72,9 +88,10 @@ static void TestMultiset_AddAllCharsCountMixed_EqualsAvlTreeCountMixed(CuTest *t
     MultisetConfig config = MULTISET_CONFIG;
     Multiset_initMultiset(&multiset, config);
 
-    char strs[256][2];
-    const size_t strsNum = sizeof(strs) / sizeof(strs[0]);
+    const size_t strsNum = 256, maxCStrLen = 2;
+    char **strs = malloc(strsNum * sizeof(char*));
     for (size_t i = 0; i < strsNum; i++) {
+        strs[i] = malloc(maxCStrLen * sizeof(char));
         strs[i][0] = (char) i;
         strs[i][1] = '\0';
     }
@@ -88,7 +105,7 @@ static void TestMultiset_AddAllCharsCountMixed_EqualsAvlTreeCountMixed(CuTest *t
                          multiset.uniqueItemsCount == countTreesUniqueItems(&multiset));
     }
 
-    Multiset_eraseMultiset(&multiset);
+    CuAssertIntEquals(tc, MULTISET_E_OK, Multiset_eraseMultiset(&multiset));
 }
 
 
